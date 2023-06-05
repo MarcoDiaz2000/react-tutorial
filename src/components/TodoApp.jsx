022ad8768a59ac5ea059cab9431196c4bb4ceb8d
MarcoDiaz2000 <md.marcodiaz2000@gmail.com>
2023-06-04 19:12:26 -0600

    Part 5

--- src/components/TodoApp.jsx
+++ src/components/TodoApp.jsx
@@ -1,9 +1,15 @@
 import Header from '@/components/Header';
 import TodosLogic from '@/components/TodosLogic';
+import Navbar from '@/components/Navbar';
+import Modal from './Modal';
+import Form from './Form';
 
 const TodoApp = () => {
   return (
     <>
+{/*       <Navbar />
+      <Modal />
+      <Form /> */}
       <Header />
       <TodosLogic />
     </>
