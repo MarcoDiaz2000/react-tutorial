d5a922b50b576905c6549a844d7d9d8fad81e904
MarcoDiaz2000 <md.marcodiaz2000@gmail.com>
2023-06-05 12:28:33 -0600

    Add redirect

--- src/routes/NotMatch.jsx
+++ src/routes/NotMatch.jsx
@@ -1,8 +1,16 @@
+import { Redirect } from 'react-router-dom';
+
 const NotMatch = () => {
+  return <Redirect to="/" />
+};
+
+export default NotMatch;
+
+/* const NotMatch = () => {
   return (
     <div>
       <p>Not found!</p>
     </div>
   );
 };
-export default NotMatch;
+export default NotMatch; */
